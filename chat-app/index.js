b9da32e6a9e0dd94f807cc02d011383ff9cdee29
AtibProgrammer <atibkhan392@gmail.com>
2023-02-25 14:57:57 +0530

    Chat Application Done

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,26 +1,37 @@
 const express= require('express')
 const bodyParser=require('body-parser')
 const fs=require('fs')
-const LocalStorage = require('node-localstorage').LocalStorage,
-localStorage = new LocalStorage('./scratch');
+// const LocalStorage = require('node-localstorage').LocalStorage,
+// localStorage = new LocalStorage('./scratch');
 app=express()
 
-app.get('/login',(req,res,next)=>{
-    res.send('<form onsubmit="localStorage.setItem("username",document.getElementById(`usa`).value)" action="/" method="POST"><input type="text" name="user" id="usa"><button type="submit">Submit</button></form>'
-    )
+
+app.use('/login',(req,res,next)=>{
+    res.send(`
+    <form onsubmit=myFunc1() action='/' method='POST' >
+    <input type='text' name='user' id='usa'> 
+    <button type='submit'>Submit</button>
+    </form>
+    <script>
+    function myFunc1(){
+        localStorage.setItem("username", document.getElementById('usa').value);
+        console.log(localStorage.getItem('username'))
+    }
+    </script>
+    `)
 })
+
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.post('/write',(req,res,next)=>{
     const b= req.body.chat
     const a=req.body.user
     fs.appendFileSync('chat.txt',`${a}:${b}`+"\n")
-    localStorage.setItem(`${a}`,b)
+    // localStorage.setItem(`${a}`,b)
     res.redirect('/')
 })
 app.use('/',(req,res,next)=>{
     const c=fs.readFileSync('chat.txt')
-    console.log(req.body);
     res.send(`
     <html>
     <body>
